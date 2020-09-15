179eb11ebdde46526ff99b1e0eef033e4edc0bea
Diego Barroso <diego-barroso@live.com>
2020-09-14 22:22:54 -0300

    tela com exibição do mapa

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import * as https from 'https';
 
 const api = axios.create({
   // baseURL: 'http://localhost:44398',
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'http://10.0.2.2:5000',
 });
 
 export default api;
